374ea924aefa67ef0deb9f0c41c9ec3eb7de0988
Logan S <lcsanderson26@outlook.com>
2026-07-13 19:55:49 -0500

    pushing format change

--- FRONTEND/src/components/IndexPage.jsx
+++ FRONTEND/src/components/IndexPage.jsx
@@ -1,12 +1,15 @@
 export default function IndexPage() {
-    console.log("Hello from React");
+  console.log("Hello from React");
 
-    fetch("/api/listings").then((res) => res.json()).then((data) => console.log("Listings data:", data));
+  fetch("/api/listings")
+    .then((res) => res.json())
+    .then((data) => console.log("Listings data:", data));
 
-    return ( 
+  return (
     <>
-    <h3> welcome to the... </h3>
-    <h1> Index Page </h1>
-    <h3> !!! </h3>
-    </> )
+      <h3> welcome to the... </h3>
+      <h1> Index Page </h1>
+      <h3> !!! </h3>
+    </>
+  );
 }
